46e03967e32723dfdd52805fe08463cee856ba10
Cied <ciedou@hotmail.fr>
2021-11-24 17:18:54 +0100

    ayy

--- app/src/main/java/com/projetdam/docdirect/MapsActivity.java
+++ app/src/main/java/com/projetdam/docdirect/MapsActivity.java
@@ -2,7 +2,9 @@ package com.projetdam.docdirect;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.media.metrics.LogSessionId;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,24 +12,48 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.projetdam.docdirect.commons.ModelDoctor;
 import com.projetdam.docdirect.databinding.ActivityMapsBinding;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private FirebaseFirestore db;
+    private CollectionReference docRef;
+    private ArrayList<ModelDoctor> listDoc;
 
+
+
+    public void init()
+    {
+        db= FirebaseFirestore.getInstance();
+        listDoc=new ArrayList<ModelDoctor>();
+        docRef=db.collection("doctors");
+
+
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        init();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     /**
@@ -42,10 +68,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        docRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for(QueryDocumentSnapshot documentSnapshot:queryDocumentSnapshots){
+                    ModelDoctor doc=documentSnapshot.toObject(ModelDoctor.class);
+                    Double lat=doc.getGeoloc().getLatitude();
+                    Double log=doc.getGeoloc().getLongitude();
+                    Log.i("cc", "onSuccess: "+doc.getGeoloc());
+                    LatLng pt = new LatLng(lat, log);
+                    mMap.addMarker(new MarkerOptions().position(pt).title("Doc"));
+                    listDoc.add(doc);
 
-         //Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                }
+
+            }
+        });
+         //Add a marker in Paris and move the camera
+        LatLng paris = new LatLng(48.864716, 2.349014);
+        mMap.addMarker(new MarkerOptions().position(paris).title("Marker in Paris"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(paris));
+
     }
 }

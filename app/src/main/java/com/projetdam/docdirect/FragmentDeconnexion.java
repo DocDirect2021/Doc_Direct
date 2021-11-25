eb40c4ed19595e89d6e2fef7f4d4d6f7b2da33e5
chitha <chtiimane@gmail.com>
2021-11-25 16:24:23 +0100

    Fragments

---
+++ app/src/main/java/com/projetdam/docdirect/FragmentDeconnexion.java
@@ -0,0 +1,27 @@
+package com.projetdam.docdirect;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentDeconnexion extends Fragment {
+
+
+
+    public FragmentDeconnexion() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_deconnexion, container, false);
+    }
+}

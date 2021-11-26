74b8a01c03244e5b47a5f20e89361fd8c3d10a07
chitha <chtiimane@gmail.com>
2021-11-26 11:29:19 +0100

    Passer le nom patient ds fragment compte, activer la deconnexion

--- app/src/main/java/com/projetdam/docdirect/FragmentCompte.java
+++ app/src/main/java/com/projetdam/docdirect/FragmentCompte.java
@@ -2,18 +2,39 @@ package com.projetdam.docdirect;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 
 public class FragmentCompte extends Fragment {
 
+    /** var **/
+
+    private TextView txtNom;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    private String userID;
+    View view;
+
+
+
 
     public FragmentCompte() {
         // Required empty public constructor
+
     }
 
 
@@ -21,6 +42,23 @@ public class FragmentCompte extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compte, container, false);
+
+          view = inflater.inflate(R.layout.fragment_compte, container, false);
+        txtNom = view.findViewById(R.id.txtNom);
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        userID = mAuth.getCurrentUser().getUid();
+
+
+        DocumentReference documentReference = db.collection("users").document(userID);
+
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                txtNom.setText(documentSnapshot.getString("nom"));
+            }
+        });
+
+        return view;
     }
 }

cd50e84e851b6fa37ea52308f06e0cbb64ff5e1c
csdevph <dev.ph@gmx.com>
2021-12-05 17:10:50 +0100

    update UtilsTimeSlot

--- app/src/main/java/com/projetdam/docdirect/commons/UtilsTimeSlot.java
+++ app/src/main/java/com/projetdam/docdirect/commons/UtilsTimeSlot.java
@@ -15,6 +15,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -26,11 +28,15 @@ public class UtilsTimeSlot {
     private static final String TAG = "TEST TEST";
     private static LocalTime time;
 
+    public static CollectionReference doctors = FirebaseFirestore.getInstance().collection("doctors");
+    public static CollectionReference consultations = FirebaseFirestore.getInstance().collection("consultations");
+    public static CollectionReference patients = FirebaseFirestore.getInstance().collection("users");
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     public static ArrayList<String> createSlots(String startTime, String endTime, int duration) {
         ArrayList<String> hours = new ArrayList<>();
 
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("H:mm");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
         LocalTime t1 = LocalTime.parse(startTime, timeFormatter);
         LocalTime t2 = LocalTime.parse(endTime, timeFormatter);
 
@@ -41,7 +47,7 @@ public class UtilsTimeSlot {
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public static void saveRdv(CollectionReference consultations, CollectionReference patients, ModelTimeSlot slot) {
+    public static void saveRdv(ModelTimeSlot slot) {
         String patientId = slot.getPatientId();
         String doctorId = slot.getDoctorId();
         String rdvId = slot.getCreateId();

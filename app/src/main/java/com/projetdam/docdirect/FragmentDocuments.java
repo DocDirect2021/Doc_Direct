1af904175f3149c86c2adca24649602b4e7aaca7
Cied <ciedou@hotmail.fr>
2021-12-09 16:26:08 +0100

    k

--- app/src/main/java/com/projetdam/docdirect/FragmentDocuments.java
+++ app/src/main/java/com/projetdam/docdirect/FragmentDocuments.java
@@ -1,21 +1,30 @@
 package com.projetdam.docdirect;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 import com.scanlibrary.ScanActivity;
 import com.scanlibrary.ScanConstants;
 
+import java.io.IOException;
+
 
 public class FragmentDocuments extends Fragment {
 
+    private ImageView scannedImageView;
 
 
+    private static final int REQUEST_CODE = 99;
     public FragmentDocuments() {
         // Required empty public constructor
     }
@@ -27,18 +36,25 @@ public class FragmentDocuments extends Fragment {
         // Inflate the layout for this fragment
         View v=inflater.inflate(R.layout.fragment_documents, container, false);
 
-        Intent intent = new Intent(getActivity(), ScanActivity.class);
-        intent.putExtra(ScanActivity.EXTRA_BRAND_IMG_RES, R.drawable.ic_crop_white_24dp); // Set image for title icon - optional
-        intent.putExtra(ScanActivity.EXTRA_TITLE, "Crop Document"); // Set title in action Bar - optional
-        intent.putExtra(ScanActivity.EXTRA_ACTION_BAR_COLOR, R.color.white); // Set title color - optional
-        intent.putExtra(ScanActivity.EXTRA_LANGUAGE, "en"); // Set language - optional
+        Intent intent = new Intent(getActivity(),DocActivity.class);
+//        intent.putExtra(ScanActivity.EXTRA_BRAND_IMG_RES, R.drawable.ic_crop_white_24dp); // Set image for title icon - optional
+//        intent.putExtra(ScanActivity.EXTRA_TITLE, "Crop Document"); // Set title in action Bar - optional
+//        intent.putExtra(ScanActivity.EXTRA_ACTION_BAR_COLOR, R.color.white); // Set title color - optional
+//        intent.putExtra(ScanActivity.EXTRA_LANGUAGE, "en"); // Set language - optional
+//        intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE,ScanConstants.OPEN_CAMERA);
         startActivity(intent);
         int REQUEST_CODE = 99;
-        int preference = ScanConstants.OPEN_CAMERA;
-        intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference);
-        startActivity(intent);
+       // startActivityForResult(intent, REQUEST_CODE);
+        scannedImageView = (ImageView) v.findViewById(R.id.scannedImage);
+
 
 
         return v;
     }
+
+    
+
+
+
+
 }

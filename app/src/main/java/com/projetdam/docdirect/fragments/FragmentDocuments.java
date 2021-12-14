c91fe8f2910dd422c98835b827bcb7cf6fc67e6a
Maxime C <maxime.cassagne@gmail.com>
2021-12-14 11:10:20 +0100

    version correction

--- app/src/main/java/com/projetdam/docdirect/fragments/FragmentDocuments.java
+++ app/src/main/java/com/projetdam/docdirect/fragments/FragmentDocuments.java
@@ -1,23 +1,13 @@
 package com.projetdam.docdirect.fragments;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
-
-import com.projetdam.docdirect.DocActivity;
-import com.scanlibrary.ScanActivity;
-import com.scanlibrary.ScanConstants;
-
-import java.io.IOException;
 
 
 import com.projetdam.docdirect.R;
@@ -39,15 +29,7 @@ public class FragmentDocuments extends Fragment {
         // Inflate the layout for this fragment
         View v=inflater.inflate(R.layout.fragment_documents, container, false);
 
-        Intent intent = new Intent(getActivity(), DocActivity.class);
-        intent.putExtra(ScanActivity.EXTRA_BRAND_IMG_RES, R.drawable.ic_crop_white_24dp); // Set image for title icon - optional
-        intent.putExtra(ScanActivity.EXTRA_TITLE, "Crop Document"); // Set title in action Bar - optional
-        intent.putExtra(ScanActivity.EXTRA_ACTION_BAR_COLOR, R.color.white); // Set title color - optional
-        intent.putExtra(ScanActivity.EXTRA_LANGUAGE, "en"); // Set language - optional
-        intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE,ScanConstants.OPEN_CAMERA);
-        startActivity(intent);
-        int REQUEST_CODE = 99;
-        startActivityForResult(intent, REQUEST_CODE);
+
 
 
 
